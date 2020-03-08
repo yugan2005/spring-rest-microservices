632c955b27a2343a33d55f37e3870fabac19ec44
Yu Gan <yugan2005@users.noreply.github.com>
2020-03-08 14:55:32 -0700

    first commit

---
+++ src/main/java/com/home/restmicroservices/RestMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.home.restmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RestMicroservicesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestMicroservicesApplication.class, args);
+  }
+}
---
+++ src/test/java/com/home/restmicroservices/RestMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.restmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class RestMicroservicesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
